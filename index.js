a60a462b11d7353f64f56ed73382b4669bade0a4
fadejevs <ralphfdesigns@gmail.com>
2022-03-14 12:38:31 +0100

    initial

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// npm init -y 
+// npm install puppeteer
+
+
+const puppeteer = require('puppeteer');
+
+( async () => { 
+
+    const browser = await puppeteer.launch({headless: true})
+    const page = await browser.newPage()
+    await page.goto('https://fadejevs.com/')
+    await page.waitForTimeout(2000)
+    await page.screenshot({ path: "photo.png"})
+
+    const grabText = await page.evaluate(() => { 
+        const text = document.querySelector('.right-side');
+        const texts = [text];
+        let array = [];
+        texts.forEach((item) => { 
+            array.push(item.innerText)
+        });
+
+        return array;
+    })
+
+
+    console.log(grabText);
+    await browser.close();
+
+})();
